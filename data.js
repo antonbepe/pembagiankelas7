2d56550a10fe204f65f5e39effcaa39996558e96
antonbepe <97140773+antonbepe@users.noreply.github.com>
2026-07-10 11:42:59 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const studentData = {
+
+    "7A":[
+        {
+            absen:1,
+            induk:"9267",
+            nama:"ABDUL GHANI",
+            jk:"L"
+        },
+        {
+            absen:2,
+            induk:"9268",
+            nama:"ABINAYA",
+            jk:"L"
+        }
+    ],
+
+    "7B":[
+        ...
+    ],
+
+    "7C":[
+        ...
+    ],
+
+    "7D":[
+        ...
+    ],
+
+    "7E":[
+        ...
+    ],
+
+    "7F":[
+        ...
+    ],
+
+    "7G":[
+        ...
+    ]
+
+};
